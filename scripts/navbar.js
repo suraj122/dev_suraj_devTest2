f4abc33e3f8ed06c37c002e81c065745d67bbbeb
Mukesh kumar <122suraj0@gmail.com>
2024-04-05 16:39:52 +0530

    add close icon to close menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,18 +1,25 @@
 const menuIcon = document.querySelector(".menu-icon");
-const navMenu = document.querySelector(".navbar-menu");
-let isMenuVisible = true;
+const close = document.querySelector(".close");
+const navMenu = document.querySelector("#navbarMenu");
+let isMenuVisible = false;
 
 // Clicking will show menu or hide it
 menuIcon.addEventListener("click", () => {
-  if (isMenuVisible) {
-    // navMenu.style.display = "inline-block";
-
+  if (!isMenuVisible) {
     navMenu.style.height = "144px";
-    // navMenu.style.opacity = "1";
-    isMenuVisible = false;
+    close.style.display = "inline-block";
+    menuIcon.style.display = "none";
+    isMenuVisible = true;
   } else {
     navMenu.style.height = "0";
-    // navMenu.style.opacity = "0";
-    isMenuVisible = true;
+    close.style.display = "none";
+    menuIcon.style.display = "inline-block";
+    isMenuVisible = false;
   }
+});
+close.addEventListener("click", () => {
+  navMenu.style.height = "0";
+  close.style.display = "none";
+  menuIcon.style.display = "inline-block";
+  isMenuVisible = false;
 });
